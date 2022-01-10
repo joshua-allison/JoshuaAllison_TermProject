dd2f14e6be88692079ea9203e5b4f7a0b5a71bfe
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-01-10 15:06:17 -0800

    Lab 1 Beta Completed
    I actually had a lot of validation already, including the necessary validation tag helper in the views and ModelState check in the controller. I did add several additional data annotations, beyond the [Required] tags that I had already. I added [Range], [RegularExpression], &[StringLength] for several attributes.
    
    I still need to go back and refactor attributes so that they are a class, so I can write the edit view in a much less repetitive way. I also need to find a way to integrate image uploading, so that icons for items don't rely on external websites, and can be uploaded from local files.

--- Models/DomainLayer/Equipment.cs
+++ Models/DomainLayer/Equipment.cs
@@ -8,10 +8,12 @@ namespace JoshuaAllison_TermProject.Models
         //EF Core will configure the database to gerenate this value.
         public int EquipmentId { get; set; }
         [Required(ErrorMessage = "Please enter the Name of the equipment.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "String length for {0} must be between {2} and {1}." )]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter the Icon URL of the equipment.")]
         public string IconURL { get; set; }
         [Required(ErrorMessage = "Please enter the Flavor Text of the equipment.")]
+        [StringLength(1000, MinimumLength = 15, ErrorMessage = "String length for {0} must be between {2} and {1}.")]
         public string FlavorText { get; set; }
         [Required(ErrorMessage = "Please enter the Subcategory of the equipment.")]
         public string ItemSubcategoryId { get; set; }
@@ -20,6 +22,7 @@ namespace JoshuaAllison_TermProject.Models
         public int WeaponArtId { get; set; }
         public WeaponArt weaponArt { get; set; }
         [Required(ErrorMessage = "Please enter the FP consumption of the equipment weapon art.")]
+        [Range(0, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int FPConsumption { get; set; }
         [Required(ErrorMessage = "Please enter the Attack Type of the equipment.")]
         public string AttackType { get; set; }
@@ -30,41 +33,50 @@ namespace JoshuaAllison_TermProject.Models
         #endregion Identifiers
         #region Attack Ratings
         [Required(ErrorMessage = "Please enter the Physical attack rating of the equipment.")]
+        [Range(0, 500, ErrorMessage = "Value for Physical Attack must be between {1} and {2}.")]
         public int PhysicalAR { get; set; }
         [Required(ErrorMessage = "Please enter the Magic attack rating of the equipment.")]
+        [Range(0, 500, ErrorMessage = "Value for Magic Attack must be between {1} and {2}.")]
         public int MagicAR { get; set; }
         [Required(ErrorMessage = "Please enter the Fire attack rating of the equipment.")]
+        [Range(0, 500, ErrorMessage = "Value for Fire Attack must be between {1} and {2}.")]
         public int FireAR { get; set; }
         [Required(ErrorMessage = "Please enter the Lightning attack rating of the equipment.")]
+        [Range(0, 500, ErrorMessage = "Value for Lightning Attack must be between {1} and {2}.")]
         public int LightningAR { get; set; }
         [Required(ErrorMessage = "Please enter the Dark attack rating of the equipment.")]
+        [Range(0, 500, ErrorMessage = "Value for Dark Attack must be between {1} and {2}.")]
         public int DarkAR { get; set; }
         #endregion Attack Ratings
         #region Defense Ratings
         [Required(ErrorMessage = "Please enter the Physical defense rating of the equipment.")]
-        [Range(0, 999, ErrorMessage = "Value for Physical Defense must be between {1} and {2}.")]
+        [Range(0, 100, ErrorMessage = "Value for Physical Defense must be between {1} and {2}.")]
         public int PhysicalDR { get; set; }
         [Required(ErrorMessage = "Please enter the Magic defense rating of the equipment.")]
-        [Range(0, 999, ErrorMessage = "Value for Magic Defense must be between {1} and {2}.")]
+        [Range(0, 100, ErrorMessage = "Value for Magic Defense must be between {1} and {2}.")]
         public int MagicDR { get; set; }
         [Required(ErrorMessage = "Please enter the Fire attack defense of the equipment.")]
-        [Range(0, 999, ErrorMessage = "Value for Fire Defense must be between {1} and {2}.")]
+        [Range(0, 100, ErrorMessage = "Value for Fire Defense must be between {1} and {2}.")]
         public int FireDR { get; set; }
         [Required(ErrorMessage = "Please enter the Lightning defense rating of the equipment.")]
-        [Range(0, 999, ErrorMessage = "Value for Lightning Defense must be between {1} and {2}.")]
+        [Range(0, 100, ErrorMessage = "Value for Lightning Defense must be between {1} and {2}.")]
         public int LightningDR { get; set; }
         [Required(ErrorMessage = "Please enter the Dark attack defense of the equipment.")]
-        [Range(0, 999, ErrorMessage = "Value for Dark Defense must be between {1} and {2}.")]
+        [Range(0, 100, ErrorMessage = "Value for Dark Defense must be between {1} and {2}.")]
         public int DarkDR { get; set; }
         #endregion Defense Ratings
         #region Stat Scaling
         [Required(ErrorMessage = "Please enter the Strength scaling of the equipment.")]
+        [RegularExpression(@"[SABCDE-]", ErrorMessage = "Strength Scaling must be expressed as \'S\', \'A\', \'B\', \'C\', \'D\', \'E\',  or \'-\', ")]
         public char StrengthScaling { get; set; }
         [Required(ErrorMessage = "Please enter the Dexterity scaling of the equipment.")]
+        [RegularExpression(@"[SABCDE-]", ErrorMessage = "Dexterity Scaling must be expressed as \'S\', \'A\', \'B\', \'C\', \'D\', \'E\',  or \'-\', ")]
         public char DexterityScaling { get; set; }
         [Required(ErrorMessage = "Please enter the Intellect scaling of the equipment.")]
+        [RegularExpression(@"[SABCDE-]", ErrorMessage = "Intellect Scaling must be expressed as \'S\', \'A\', \'B\', \'C\', \'D\', \'E\',  or \'-\', ")]
         public char IntellectScaling { get; set; }
         [Required(ErrorMessage = "Please enter the Faith scaling of the equipment.")]
+        [RegularExpression(@"[SABCDE-]", ErrorMessage = "Faith Scaling must be expressed as \'S\', \'A\', \'B\', \'C\', \'D\', \'E\',  or \'-\', ")]
         public char FaithScaling { get; set; }
         #endregion Stat Scaling
         #region Stat Requirements
